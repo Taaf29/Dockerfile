61a8f6f4cc450d31259d3e47759a5e703167b351
Mouss <almoustaphaekhizi@gmail.com>
2022-06-14 12:23:34 +0200

    3ème

--- test_functions.py
+++ test_functions.py
@@ -1,3 +1,4 @@
+from unittest import result
 from main import count_char, check_if_maj, check_if_special, check_if_valid_password
 
 def test_count_char():
@@ -5,3 +6,29 @@ def test_count_char():
     expected = 7
     result = count_char(input)
     assert expected == result
+
+def test1_maj ():
+    input1="hi" 
+    input2="yo"
+
+    expected1=True
+    expected2=False
+
+    result1=check_if_maj(input1)
+    result2=check_if_maj(input2)
+    
+    assert expected1==result1
+    assert expected2==result2
+
+def test2_special():
+    input1="hi sir" 
+    input2="yohoho"
+
+    expected1=True
+    expected2=False
+
+    result1=check_if_special(input1)
+    result2=check_if_special(input2)
+
+    assert expected1==result1
+    assert expected2==result2
